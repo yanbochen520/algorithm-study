d76ba4e9c7721e77e003e9b62dd60118ffb7bdf6
ybchen1 <ybchen1@chinaums.com>
2023-03-10 09:10:08 +0800

    first commit

---
+++ src/main/java/org/ybchen/App.java
@@ -0,0 +1,13 @@
+package org.ybchen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
